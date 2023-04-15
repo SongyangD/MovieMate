0a9018b1c3a46b0e57ce7c2f97d24f7591cfd1f1
SongyangD <47569565+SongyangD@users.noreply.github.com>
2023-04-14 20:20:28 -0400

    !!!finalized server!!!
    all finalized server connections

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ app.use(cors({
 // We use express to define our various API endpoints and
 // provide their handlers that we implemented in routes.js
 
-// app.get('/song/:song_id', routes.song);
 app.get('/movies/:movie_id', routes.movie);
 app.get('/movies', routes.movies);
 
@@ -19,26 +18,19 @@ app.get('/search_movies', routes.search_movies);
 app.get('/people/:person_id', routes.person);
 app.get('/people', routes.people);
 app.get('/search_people', routes.search_people);
-app.get('/oscar_movies', routes.oscar_movies);
-app.get('/search_oscar_winner', routes.search_oscar_winner);
 app.get('/movie_people/:movie_id', routes.movie_people);
-app.get('/top20_movies', routes.top20_movies);
-
+app.get('/related_actors/:id', routes.related_actors);
+app.get('/top10_rated_oscar_movies', routes.top10_rated_oscar_movies);
 app.get('/movie_count', routes.movie_count);
-app.get('/oscar_ranking', routes.oscar_ranking);
 app.get('/avg_vote_person/:person_id', routes.avg_vote_person);
-// app.get('/album_songs/:album_id', routes.album_songs);
-// app.get('/top_songs', routes.top_songs);
-// app.get('/top_albums', routes.top_albums);
-// app.get('/author/:type', routes.author);
-// app.get('/random', routes.random);
-
-// app.get('/album/:album_id', routes.album);
-// app.get('/albums', routes.albums);
-// app.get('/search_songs', routes.search_songs);
 app.get('/oscar_recommend', routes.oscarMovieRecommended);
 app.get('/recent10genre/:genre', routes.recent10genre);
 app.get('/top10language/:language', routes.top10language);
+app.get('/oscar', routes.search_oscar_people);
+app.get('/search_oscar_filter', routes.search_oscar_filter);
+app.get('/search_won', routes.search_won);
+app.get('/top_oscar_director', routes.top_oscar_director);
+app.get('/stats', routes.oscar_decade);
 
 
 app.listen(config.server_port, () => {
